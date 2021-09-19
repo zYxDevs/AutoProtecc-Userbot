5ab562f512d673a51f50a67de81cd66cc686f234
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 01:58:22 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,17 +9,17 @@
 
 
 import os
-import sys
-
-BOT_LIST = "792028928 1232515770 1964681186"
 
 class Config:
 # Get these values from my.telegram.org
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
+
 # Get this from https://replit.com/@YogaPranata1/PyroStringGen
     STRING_SESSION = os.environ.get("STRING_SESSION")
+
 # Time delay in seconds so that it does not seep suspecious. Default = 5s
     DELAY = int(os.environ.get("DELAY", "5"))
+
 # Waifu, Servant, Husbando bot id.
     BOT_LIST = {int(x) for x in os.environ.get("BOT_LIST").split()}
