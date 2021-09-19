d202df624ed2715e19c86e7b565032b931594802
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-19 20:20:15 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,18 +1,18 @@
 # # -*- coding: utf-8 -*-
 
-# @author: Kgf123
+# @author: zYxDevs
 # Licensed Under 'GPU v3.0'
-# Copyright (C) 2021 https://github.com/Kgf123
-# This file is part of Project-Autowaifu.
-# Pproject-Autowaifu must not be copied and/or distributed without the express permission of Kgf123.
-# All rights resrved
+# Copyright (C) 2021 https://github.com/zYxDevs
+# This file is part of Project-AutoProtecc.
+# Pproject-AutoProtecc must not be copied and/or distributed without the express permission of zYxDevs.
+# All rights reserved.
 
 
 import os
 
 
 # Get these values from my.telegram.org
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("APP_ID"))
 API_HASH = os.environ.get("API_HASH")
-# Get this from https://replit.com/@TeamUltroid/UltroidStringSession
+# Get this from https://replit.com/@YogaPranata1/PyroStringGen
 STRING_SESSION = os.environ.get("STRING_SESSION")
