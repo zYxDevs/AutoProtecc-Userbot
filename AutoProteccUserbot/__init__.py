38f9a1ce98d0e421a23f95b1abed37dc496c2e53
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 09:40:13 +0700

    Update __init__.py

--- AutoProteccUserbot/__init__.py
+++ AutoProteccUserbot/__init__.py
@@ -4,8 +4,8 @@ from pyrogram import Client, errors
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
-STRING_SESSION = os.environ.get("STRING_SESSION", "waifu")
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
 DELAY = os.environ.get("DELAY", None)
 BOT_LIST = {int(x) for x in os.environ.get("BOT_LIST", "").split()}
 
-Waifu = Client("waifu", api_id=API_ID, api_hash=API_HASH, session_name=STRING_SESSION)
+Waifu = Client(STRING_SESSION, api_id=API_ID, api_hash=API_HASH)
