0f3064ea90e44ff437a4d56be1f9da27dad6383a
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 09:54:23 +0700

    Update __init__.py

--- AutoProteccUserbot/__init__.py
+++ AutoProteccUserbot/__init__.py
@@ -8,4 +8,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", None)
 DELAY = os.environ.get("DELAY", None)
 BOT_LIST = {int(x) for x in os.environ.get("BOT_LIST", "").split()}
 
-Waifu = Client(STRING_SESSION, api_id=API_ID, api_hash=API_HASH)
+if STRING_SESSION:
+    Waifu = Client(STRING_SESSION, api_id=API_ID, api_hash=API_HASH)
+else:
+    Waifu = Client("userbot", api_id=API_ID, api_hash=API_HASH)
