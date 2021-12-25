debb53514ae25d22abc55a7b2cc2c3c174a17c8c
Yoga Pranata <92792431+TheSungJinWoo@users.noreply.github.com>
2021-12-26 02:06:43 +0700

    incomplete
    feel sleepy

--- AutoWaifu.py
+++ AutoWaifu.py
@@ -1,8 +1,12 @@
 import time
+import io
 import os
+import urllib
 import requests
+import aiohttp
 
-from bs4 import BeautifulSoup as bs
+from re import findall
+from bs4 import BeautifulSoup
 from aiohttp import ClientSession
 from asyncio import (
     gather,
@@ -18,10 +22,10 @@ from pyrogram import (
 
 
 API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-STRING_SESSION = os.environ.get("STRING_SESSION")
+API_HASH = str(os.environ.get("API_HASH"))
+STRING_SESSION = str(os.environ.get("STRING_SESSION"))
 DELAY = int(os.environ.get("DELAY"))
-BOT_LIST = 
+BOT_LIST = {int(x) for x in os.environ.get("BOT_LIST").split()}
 
 
 Waifu = Client(":memory:",
@@ -31,31 +35,51 @@ Waifu = Client(":memory:",
 )
 
 
-@Waifu.on_message()
+u_ = """Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"""
+headers_ = [("User-agent", u_)]
+
+
+async def ParseSauce(googleurl):
+    async with aiohttp.ClientSession(headers=headers_) as session:
+      async with session.get(googleurl) as resp:
+          source = await resp.read()
+    soup = BeautifulSoup(source, "html.parser")
+    results = {"similar_images": "", "best_guess": ""}
+    try:
+        for similar_image in soup.findAll("input", {"class": "gLFyf"}):
+            url = "https://www.google.com/search?tbm=isch&q=" + urllib.parse.quote_plus(
+                similar_image.get("value")
+            )
+            results["similar_images"] = url
+    except BaseException:
+        pass
+    for best_guess in soup.findAll("div", attrs={"class": "r5a77d"}):
+        results["best_guess"] = best_guess.get_text()
+    return results
+
+
+def get_data(img):
+    searchUrl = "https://www.google.com/searchbyimage/upload"
+    file_img = {"encoded_image": (img, open(img, "rb")), "image_content": ""}
+    response = requests.post(searchUrl, files=file_img, allow_redirects=False)
+    if os.path.exists(img):
+        os.remove(img)
+    if response.status_code == 400:
+        return
+    return response.headers["Location"]
+
+
+@Waifu.add_handler(MessageHandler(filters.group & ~filters.edited & ~filters.forward), group=0)
 async def autowaifu(client, message):
-    if message.photo and message.user.id in BOT_LIST:
-        dl = await Waifu.download_media(message, "resources/")
-        file = {"encoded_image": (dl, open(dl, "rb"))}
-        grs = requests.post(
-            "https://www.google.com/searchbyimage/upload",
-            files=file,
-            allow_redirects=False,
-        )
-        loc = grs.headers.get("location")
-        response = requests.get(
-            loc,
-            headers={
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
-            },
-        )
-        xx = bs(response.text, "html.parser")
-        div = xx.find_all("div", {"class": "r5a77d"})[0]
-        alls = div.find("a")
-        text = alls.text
-        time.sleep(DELAY)
-        send = await Waifu.send_message(message.chat.id, f"/protecc {text}")
-        await sleep(5)
-        os.remove(dl)
+    if message.photo and message.from_user.id in BOT_LIST:
+        img = await message.download()
+        fetchUrl = await get_data(img)
+        match = await ParseSauce(fetchUrl + "&preferences?hl=en&fg=1#languages")
+        guess = match["best_guess"]
+        await sleep(DELAY)
+        kek = await message.reply_text(f"/protecc {guess}")
+        await sleep(DELAY)
+        await kek.delete()
 
 
 async def main():
