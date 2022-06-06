6ca76d5b8708cae15e0c31c4c4c608d9077ff651
Yoga Pranata <zyxdevs@otakufanshare.eu.org>
2022-06-07 00:40:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class config:
   API_HASH = ""  # Get it from my.telegram.org
   STRING_SESSION = ""  # Get it from t.me/GetStringRobot (Choose Pyrogram)
   DELAY = 0  # Waif for x before send message
-  BOT_LIST = ""  # Harem, Servant or Husbando bot ids
+  BOT_LIST = ""  # Harem, Servant or Husbando bot ids separated by space
   DATABASE_URL = ""  # For later, leave it empty now
