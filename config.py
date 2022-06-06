e761d893273d8c84feacbf3c38a2f762ed4f8ac8
zYxDevs <zYxDevs@users.noreply.github.com>
2022-06-06 17:46:35 +0000

    Automated code formatting
    Signed-off-by: zYxDevs <45356875+zYxDevs@users.noreply.github.com>

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ class config:
     API_HASH = ""  # Get it from my.telegram.org
     STRING_SESSION = ""  # Get it from t.me/GetStringRobot (Choose Pyrogram)
     DELAY = 0  # Waif for x before send message
-    BOT_LIST = "792028928 1733263647"  # Harem, Servant or Husbando bot ids separated by space
+    BOT_LIST = (
+        "792028928 1733263647"  # Harem, Servant or Husbando bot ids separated by space
+    )
     DATABASE_URL = ""  # For later, leave it empty now
